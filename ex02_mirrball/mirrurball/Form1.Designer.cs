116b76d3ff72193c2ee5226c9b8fd6982662d60f
Fergus4506 <115529614+Fergus4506@users.noreply.github.com>
2023-09-26 22:21:04 +0800

    [upload] make circle not gap and need to make new part of function

--- ex02_mirrball/mirrurball/Form1.Designer.cs
+++ ex02_mirrball/mirrurball/Form1.Designer.cs
@@ -28,22 +28,42 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.colorDialog2 = new System.Windows.Forms.ColorDialog();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 20F);
+            this.label1.Location = new System.Drawing.Point(28, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 27);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "點數數量";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
             this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ColorDialog colorDialog2;
     }
 }
 
--- ex02_mirrball/mirrurball/Form1.cs
+++ ex02_mirrball/mirrurball/Form1.cs
@@ -20,6 +20,40 @@ namespace mirrurball
         Graphics g;
         int size = 20,number=1;
         double xx, yy, deg;
+        Color bkColor= Color.White;
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Add)
+            {
+                number *= 2;
+            }
+            else if (e.KeyData == Keys.Subtract)
+            {
+                number /= 2;
+            }
+            else if (e.KeyData == Keys.C)
+            {
+                colorDialog1.ShowDialog();
+                set_color = colorDialog1.Color;
+            }
+            else if (e.KeyData == Keys.Up)
+            {
+                size += 5;
+
+            }
+            else if (e.KeyData == Keys.Down) {
+                if (size > 5) {
+                    size -= 5;
+                }
+            }
+            else if(e.KeyData == Keys.V){
+                colorDialog2.ShowDialog();
+                bkColor=colorDialog2.Color;
+
+            }
+        }
+
         Color set_color = Color.Red;
 
         private void Form1_Load(object sender, EventArgs e)
@@ -27,14 +61,25 @@ namespace mirrurball
             this.Width = 800;
             this.Height = 600;
             MessageBox.Show("formWidth="+this.Width+"\nformHeight="+this.Height);
+            this.DoubleBuffered=true;
         }
 
         private void Form1_MouseMove(object sender, MouseEventArgs e)
         {
-            int xBias = 42;
-            g = this.CreateGraphics();
-            g.Clear(Color.White);
-            g.FillEllipse(new SolidBrush(set_color), e.X-size/2, e.Y-size/2, size, size);
+            PointF center = new PointF(this.ClientSize.Width/2,this.ClientSize.Height/2);
+            g = CreateGraphics();
+            g.Clear(bkColor);
+            for (int i = 0; i < number; i++) { 
+                double x =(double) (e.X-center.X);
+                double y = (double)(e.Y - center.Y);
+                double temp =2 * Math.PI  * i / number;
+                //Math.Cos(temp) * x - Math.Sin(temp) * y,Math.Sin(temp) * x + Math.Cos(temp) * y
+                decimal new_x = (decimal)(Math.Cos(temp) * x - Math.Sin(temp) * y);
+                decimal new_y = (decimal)(Math.Sin(temp) * x + Math.Cos(temp) * y);
+                new_x +=(decimal) center.X;
+                new_y += (decimal)center.Y;
+                g.FillEllipse(new SolidBrush(set_color),(float)new_x-size/2,(float)new_y-size/2,size,size);
+            }
         }
     }
 }
