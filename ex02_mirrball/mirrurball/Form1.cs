f073ffca4c1b6b5647146bfc7b8d5f14a8a4ca3c
Yang Dunjie <115529614+Fergus4506@users.noreply.github.com>
2023-09-26 15:03:38 +0800

    [new] ex02 temp upload

---
+++ ex02_mirrball/mirrurball/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mirrurball
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Graphics g;
+        int size = 20,number=1;
+        double xx, yy, deg;
+        Color set_color = Color.Red;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Width = 800;
+            this.Height = 600;
+            MessageBox.Show("formWidth="+this.Width+"\nformHeight="+this.Height);
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            int xBias = 42;
+            g = this.CreateGraphics();
+            g.Clear(Color.White);
+            g.FillEllipse(new SolidBrush(set_color), e.X-size/2, e.Y-size/2, size, size);
+        }
+    }
+}
