33332809d7dc7831439bed28c1745c64816e77ec
Fergus4506 <115529614+Fergus4506@users.noreply.github.com>
2023-09-30 15:10:05 +0800

    [upload] hw2 version 1

--- ex02_mirrball/mirrurball/Form1.Designer.cs
+++ ex02_mirrball/mirrurball/Form1.Designer.cs
@@ -31,24 +31,38 @@
             this.colorDialog1 = new System.Windows.Forms.ColorDialog();
             this.label1 = new System.Windows.Forms.Label();
             this.colorDialog2 = new System.Windows.Forms.ColorDialog();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("新細明體", 20F);
+            this.label1.Font = new System.Drawing.Font("新細明體", 18F);
             this.label1.Location = new System.Drawing.Point(28, 23);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(120, 27);
+            this.label1.Size = new System.Drawing.Size(106, 24);
             this.label1.TabIndex = 0;
             this.label1.Text = "點數數量";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("新細明體", 12F);
+            this.button1.Location = new System.Drawing.Point(629, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 50);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "變換畫線方式(也可按K改變)";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
+            this.KeyPreview = true;
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -64,6 +78,7 @@
         private System.Windows.Forms.ColorDialog colorDialog1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ColorDialog colorDialog2;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- ex02_mirrball/mirrurball/Form1.cs
+++ ex02_mirrball/mirrurball/Form1.cs
@@ -21,16 +21,29 @@ namespace mirrurball
         int size = 20,number=1;
         double xx, yy, deg;
         Color bkColor= Color.White;
+        bool draw=false,dl=false,dk=false;
+
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (dk) dk = false; else dk = true;
+        }
 
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
             if (e.KeyData == Keys.Add)
             {
                 number *= 2;
+                label1.Text = "目前有" + number.ToString() + "個點";
             }
             else if (e.KeyData == Keys.Subtract)
             {
-                number /= 2;
+                if (number > 1)
+                {
+                    number /= 2;
+                }
+                label1.Text = "目前有" + number.ToString() + "個點";
+
             }
             else if (e.KeyData == Keys.C)
             {
@@ -40,17 +53,34 @@ namespace mirrurball
             else if (e.KeyData == Keys.Up)
             {
                 size += 5;
+                label1.Text = "目前點的大小" + size.ToString();
 
             }
-            else if (e.KeyData == Keys.Down) {
-                if (size > 5) {
+            else if (e.KeyData == Keys.Down)
+            {
+                if (size > 5)
+                {
                     size -= 5;
                 }
+                label1.Text = "目前點的大小" + size.ToString();
             }
-            else if(e.KeyData == Keys.V){
+            else if (e.KeyData == Keys.V)
+            {
                 colorDialog2.ShowDialog();
-                bkColor=colorDialog2.Color;
-
+                bkColor = colorDialog2.Color;
+            }
+            else if (e.KeyData == Keys.D)
+            {
+                if (draw) draw = false;
+                else draw = true;
+            }
+            else if (e.KeyData == Keys.X)
+            {
+                if (dl) dl = false;
+                else dl = true;
+            }
+            else if (e.KeyData == Keys.K) {
+                if (dk) dk = false; else dk = true;
             }
         }
 
@@ -66,9 +96,10 @@ namespace mirrurball
 
         private void Form1_MouseMove(object sender, MouseEventArgs e)
         {
+            decimal[,] allPoint = new decimal[number,2];
             PointF center = new PointF(this.ClientSize.Width/2,this.ClientSize.Height/2);
             g = CreateGraphics();
-            g.Clear(bkColor);
+            if (draw==false) g.Clear(bkColor);
             for (int i = 0; i < number; i++) { 
                 double x =(double) (e.X-center.X);
                 double y = (double)(e.Y - center.Y);
@@ -79,7 +110,32 @@ namespace mirrurball
                 new_x +=(decimal) center.X;
                 new_y += (decimal)center.Y;
                 g.FillEllipse(new SolidBrush(set_color),(float)new_x-size/2,(float)new_y-size/2,size,size);
+                allPoint[i, 0] = new_x;
+                allPoint[i, 1] = new_y;
             }
+            if (dl && number != 1) {
+                Draw_line(allPoint,number);
+            }
+
+        }
+        private void Draw_line(decimal[,] allPoint,int number) {
+            if (dk)
+            {
+                for (int i = 0; i < (int)number / 2; i++)
+                {
+                    g.DrawLine(new Pen(set_color, size), (float)allPoint[i, 0], (float)allPoint[i, 1], (float)allPoint[(int)(number / 2) + i, 0], (float)allPoint[(int)(number / 2) + i, 1]);
+                }
+            }
+            else {
+                for (int i = 0; i < (int)number / 2; i++)
+                {
+                    g.DrawLine(new Pen(set_color, size), (float)allPoint[i, 0], (float)allPoint[i, 1], (float)allPoint[number-i-1, 0], (float)allPoint[number- i-1, 1]);
+                }
+            }
+            
+           
+            
+            
         }
     }
 }
