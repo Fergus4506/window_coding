c674861d72b9a14b009290c47cb033772e14c871
Fergus4506 <115529614+Fergus4506@users.noreply.github.com>
2023-10-04 11:17:33 +0800

    [upload] count clock have some problem need to solve

--- ex03_clock/ex03_clock/Form1.cs
+++ ex03_clock/ex03_clock/Form1.cs
@@ -19,8 +19,10 @@ namespace ex03_clock
         int second = DateTime.Now.Second;
         int minute = DateTime.Now.Minute;
         int hour = DateTime.Now.Hour*5;
-        string back_count;
+        string back_count,set_time;
         bool back_check=false;
+        int count_sec = 0,count_min=0,count_hur=0;
+        
 
         Boolean textboxHasText = false;//判断输入框是否有文本
 
@@ -32,6 +34,13 @@ namespace ex03_clock
 
             textBox1.ForeColor = Color.Black;
         }
+        private void Textbox2_Enter(object sender, EventArgs e)
+        {
+            if (textboxHasText == false)
+                textBox2.Text = "";
+
+            textBox2.ForeColor = Color.Black;
+        }
         //textbox失去焦点
         private void Textbox1_Leave(object sender, EventArgs e)
         {
@@ -39,6 +48,17 @@ namespace ex03_clock
             {
                 textBox1.Text = "倒數器(輸入格式為:hh/mm/ss)";
                 textBox1.ForeColor = Color.DarkGray;
+                textboxHasText = false;
+            }
+            else
+                textboxHasText = true;
+        }
+        private void Textbox2_Leave(object sender, EventArgs e)
+        {
+            if (textBox2.Text == "")
+            {
+                textBox2.Text = "設定時間(輸入格式為:hh/mm/ss)";
+                textBox2.ForeColor = Color.DarkGray;
                 textboxHasText = false;
             }
             else
@@ -56,6 +76,9 @@ namespace ex03_clock
             doPaint();
             textBox1.LostFocus += new EventHandler(Textbox1_Leave); //失去焦点后发生事件
             textBox1.GotFocus += new EventHandler(Textbox1_Enter);  //获取焦点前发生事件
+            textBox2.LostFocus += new EventHandler(Textbox2_Leave); //失去焦点后发生事件
+            textBox2.GotFocus += new EventHandler(Textbox2_Enter);  //获取焦点前发生事件
+            this.DoubleBuffered = true;
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -69,42 +92,108 @@ namespace ex03_clock
             }
             else { 
                 back_check= true;
-                hour = int.Parse(split_back[0]);
+                hour = int.Parse(split_back[0])*5;
                 minute = int.Parse(split_back[1]);
                 second = int.Parse(split_back[2]);
                 MessageBox.Show(hour+""+minute+""+second);
             }
         }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            set_time = textBox2.Text;
+            String[] split_set = set_time.Split('/');
+            if (back_check != true && split_set.Length == 3 && split_set[0] != "設定時間(輸入格式為:hh")
+            {
+                hour = int.Parse(split_set[0])*5;
+                minute = int.Parse(split_set[1]);
+                second = int.Parse(split_set[2]);
+            }
+            else {
+                if (back_check) {
+                    MessageBox.Show("正在倒數，不可設定時間，要改變倒數時間請從上面修改");
+                }
+                else if (split_set.Length != 3 || split_set[0] == "設定時間(輸入格式為:hh") {
+                    MessageBox.Show("請輸入正確格式⊙﹏⊙∥");
+                }
+                textBox2.Text = "";
+            }
+        }
 
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            count_sec++;
+            if (count_sec == 60)
+            {
+                count_min++;
+                count_sec = 0;
+            }
+            if (count_min == 60)
+            {
+                count_hur += 5;
+                count_min = 0;
+            }
+            if (count_hur == 24)
+            {
+                count_hur = 0;
+            }
+            doPaint();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (back_check)
+            {
+                if (timer1.Enabled)
+                {
+                    MessageBox.Show("正在倒數，目前暫停的時間是倒數時間");
+                    timer1.Enabled = false;
+                }
+                else
+                {
+                    MessageBox.Show("開始倒數了\\(@^0^@)/");
+                    timer1.Enabled = true;
+                }
+                
+            }
+            else {
+                if (timer1.Enabled)
+                {
+                    MessageBox.Show("時鐘暫停");
+                    timer1.Enabled = false;
+                }
+                else {
+                    MessageBox.Show("時鐘暫停結束");
+                    timer1.Enabled = true;
+                }
+                
+            }
+        }
         private void timer1_Tick(object sender, EventArgs e)
         {
             if (back_check)
             {
                 second--;
-                
-                if (second == 0)
+                if (second <= 0 && minute <= 0 && hour <= 0)
                 {
-                    if (second == 0 && minute == 0 && hour == 0)
+                    MessageBox.Show("倒數結束");
+                    back_check = false;
+                    second = DateTime.Now.Second;
+                    minute = DateTime.Now.Minute;
+                    hour = DateTime.Now.Hour * 5;
+                }
+                else {
+
+                    if (minute == 0 && second <= 0)
                     {
-                        MessageBox.Show("倒數結束");
-                        back_check = false;
-                        second = DateTime.Now.Second;
-                        minute = DateTime.Now.Minute;
-                        hour = DateTime.Now.Hour * 5;
-                    }
-                    else {
-                        minute--;
-                        if (minute == 0 && second == 0)
-                        {
-                            hour -= 5;
-                            minute = 60;
-                        }
+                        hour -= 5;
+                        minute = 59;
                         second = 60;
                     }
-                    
+                    else if (second <= 0) {
+                        minute--;
+                        second = 60;
+                    } 
                 }
-                
-                
             }
             else {
                 second++;
@@ -132,54 +221,76 @@ namespace ex03_clock
             
             g.DrawEllipse(new Pen(Color.Black), 390, 50, 140, 140);
             g.FillEllipse(new SolidBrush(Color.Black),455,115,10,10);
+            print_clock_scale(x1,y1,g);
             
-            for (int i = 1; i <= 60; i++) {
-                float drRadian = (float)i * pi / 180;
-                int x = (int)(x1 + 55 * (float)Math.Sin(6 * drRadian));
-                int y = (int)(y1 - 55 * (float)(Math.Cos(6 * drRadian)));
-                int x_h = (int)(x1 + 70 * (float)Math.Sin(6 * drRadian));
-                int y_h = (int)(y1 - 70 * (float)(Math.Cos(6 * drRadian)));
-                int x_helf = (int)(x1 + 65 * (float)Math.Sin(6 * drRadian));
-                int y_helf = (int)(y1 - 65 * (float)(Math.Cos(6 * drRadian)));
-                int x_helf_L = (int)(x1 + 63 * (float)Math.Sin(6 * drRadian));
-                int y_helf_L = (int)(y1 - 63 * (float)(Math.Cos(6 * drRadian)));
-
-                if (i % 5 == 0)
-                {
-                    if (i == 60)
-                        g.DrawString((i / 5).ToString(), new Font("Times New Roman", 10, FontStyle.Bold | FontStyle.Italic), new SolidBrush(Color.Green), new Point(x - 10, y -7));
-                    else
-                        g.DrawString((i / 5).ToString(), new Font("Times New Roman", 10, FontStyle.Bold | FontStyle.Italic), new SolidBrush(Color.Green), new Point(x - 5, y - 7));
-                    g.DrawLine(new Pen(Color.Black, 2), x_h, y_h, x_helf_L, y_helf_L);
-                }
-                else {
-                    g.DrawLine(new Pen(Color.Black, 1), x_h, y_h, x_helf, y_helf);
-                }
-                
-            }
-                g.DrawEllipse(new Pen(Color.Black), 220, 50, 140, 140);
-            g.FillEllipse(new SolidBrush(Color.Black), 285, 115, 10, 10);
+            g.DrawEllipse(new Pen(Color.Black), 390, 200, 140, 140);
+            g.FillEllipse(new SolidBrush(Color.Black), 455, 265, 10, 10);
+            print_clock_scale(460.0F,270.0F,g);
 
             float secRadian = (float)second * pi / 180;
             float midRadian = (float)minute * pi / 180;
             float hurRadian = (float)hour * pi / 180;
 
-            x2 = x1 + 60 * (float)Math.Sin(6 * secRadian);
-            y2 = y1 - 60 * (float)(Math.Cos(6 * secRadian));
-            g.DrawLine(new Pen(Color.Red), x1, y1, x2, y2);
+            draw_clock_line(hurRadian,midRadian,secRadian,x1,y1,g);
 
-            x2 = x1 + 45 * (float)Math.Sin(6 * midRadian);
-            y2 = y1 - 45 * (float)(Math.Cos(6 * midRadian));
-            g.DrawLine(new Pen(Color.Brown,5), x1, y1, x2, y2);
+            secRadian = (float)count_sec * pi / 180;
+            midRadian = (float)count_min * pi / 180;
+            hurRadian = (float)count_hur * pi / 180;
 
-            x2 = x1 + 35 * (float)Math.Sin(6 * hurRadian);
-            y2 = y1 - 35* (float)(Math.Cos(6 * hurRadian));
-            g.DrawLine(new Pen(Color.Black,10), x1, y1, x2, y2);
+            draw_clock_line(hurRadian, midRadian, secRadian, 460.0F, 270.0F, g);
+
+
 
             /*g.DrawString("12",new Font("Times New Roman",10,FontStyle.Bold | FontStyle.Italic),new SolidBrush(Color.Green),new Point(390+65,50));
             g.DrawString("6", new Font("Times New Roman", 10, FontStyle.Bold | FontStyle.Italic), new SolidBrush(Color.Green), new Point(390 + 65, 50+140-15));
             g.DrawString("3", new Font("Times New Roman", 10, FontStyle.Bold | FontStyle.Italic), new SolidBrush(Color.Green), new Point(390 + 140-15, 50+140/2-10));
             g.DrawString("9", new Font("Times New Roman", 10, FontStyle.Bold | FontStyle.Italic), new SolidBrush(Color.Green), new Point(390+5, 50+140/2-10));*/
+        }
+        void do_count_paint() {
+            Graphics g = this.CreateGraphics();
+            g.Clear(Color.White);
+
+        }
+        void print_clock_scale(float centerX,float centerY,Graphics g) {
+            for (int i = 1; i <= 60; i++)
+            {
+                float drRadian = (float)i * pi / 180;
+                int x = (int)(centerX + 55 * (float)Math.Sin(6 * drRadian));
+                int y = (int)(centerY - 55 * (float)(Math.Cos(6 * drRadian)));
+                int x_h = (int)(centerX + 70 * (float)Math.Sin(6 * drRadian));
+                int y_h = (int)(centerY - 70 * (float)(Math.Cos(6 * drRadian)));
+                int x_helf = (int)(centerX + 65 * (float)Math.Sin(6 * drRadian));
+                int y_helf = (int)(centerY - 65 * (float)(Math.Cos(6 * drRadian)));
+                int x_helf_L = (int)(centerX + 63 * (float)Math.Sin(6 * drRadian));
+                int y_helf_L = (int)(centerY - 63 * (float)(Math.Cos(6 * drRadian)));
+
+                if (i % 5 == 0)
+                {
+                    if (i == 60)
+                        g.DrawString((i / 5).ToString(), new Font("Times New Roman", 10, FontStyle.Bold | FontStyle.Italic), new SolidBrush(Color.Green), new Point(x - 10, y - 7));
+                    else
+                        g.DrawString((i / 5).ToString(), new Font("Times New Roman", 10, FontStyle.Bold | FontStyle.Italic), new SolidBrush(Color.Green), new Point(x - 5, y - 7));
+                    g.DrawLine(new Pen(Color.Black, 2), x_h, y_h, x_helf_L, y_helf_L);
+                }
+                else
+                {
+                    g.DrawLine(new Pen(Color.Black, 1), x_h, y_h, x_helf, y_helf);
+                }
+
+            }
+        }
+        void draw_clock_line(float hurRadian, float midRadian, float secRadian, float centerX, float centerY, Graphics g) {
+            x2 = centerX + 60 * (float)Math.Sin(6 * secRadian);
+            y2 = centerY - 60 * (float)(Math.Cos(6 * secRadian));
+            g.DrawLine(new Pen(Color.Red), centerX, centerY, x2, y2);
+
+            x2 = centerX + 45 * (float)Math.Sin(6 * midRadian);
+            y2 = centerY - 45 * (float)(Math.Cos(6 * midRadian));
+            g.DrawLine(new Pen(Color.Brown, 5), centerX, centerY, x2, y2);
+
+            x2 = centerX + 35 * (float)Math.Sin(6 * hurRadian);
+            y2 = centerY - 35 * (float)(Math.Cos(6 * hurRadian));
+            g.DrawLine(new Pen(Color.Black, 10), centerX, centerY, x2, y2);
         }
     }
 }
