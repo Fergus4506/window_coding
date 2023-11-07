e596e290eca4d3719ac4eff01e5aa1a13c7fae67
Yang Dunjie <115529614+Fergus4506@users.noreply.github.com>
2023-11-07 15:34:02 +0800

    [upload] ex07 std fun AC

---
+++ ex07_tar/ex07_tar/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace ex07_tar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.start = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.times = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(97, 12);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(75, 23);
+            this.start.TabIndex = 0;
+            this.start.Text = "開始";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "層數";
+            // 
+            // times
+            // 
+            this.times.Location = new System.Drawing.Point(52, 12);
+            this.times.Name = "times";
+            this.times.Size = new System.Drawing.Size(39, 22);
+            this.times.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.times);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.start);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox times;
+    }
+}
+
---
+++ ex07_tar/ex07_tar/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ex07_tar
+{
+    public partial class Form1 : Form
+    {
+        Graphics g;
+        Pen pen = new Pen(Color.Black);
+        SolidBrush brush = new SolidBrush(Color.Black);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Width = 900;
+            this.Height = 1000;
+            times.Text = "2";
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            int cnt = Int32.Parse(times.Text);
+            //MessageBox.Show(cnt.ToString());
+            g = this.CreateGraphics();
+            g.Clear(Color.White);
+
+            Point[] arr = new Point[3] { new Point(400, 100), new Point(0, 900), new Point(800, 900) };
+            g.DrawPolygon(pen,arr);
+            g.FillPolygon(brush,arr);
+            if (cnt > 0)
+                draw_wit_tar(g,arr,cnt);
+        }
+        void draw_wit_tar(Graphics g,Point[] arr,int stop) {
+            Point[] temp = new Point[3] { new Point((arr[0].X + arr[1].X) / 2, (arr[0].Y + arr[1].Y) / 2), new Point((arr[1].X + arr[2].X) / 2, (arr[1].Y + arr[2].Y) / 2), new Point((arr[2].X + arr[0].X) / 2, (arr[2].Y + arr[0].Y) / 2) };
+            g.DrawPolygon(new Pen(Color.White), temp);
+            g.FillPolygon(new SolidBrush(Color.White), temp);
+            if (stop - 1 != 0) {
+                for (int i = 0; i < 3; i++) {
+                    if(i==0)
+                        draw_wit_tar(g,new Point[3]{arr[0],temp[0],temp[2]},stop-1);
+                    else if(i==1)
+                        draw_wit_tar(g, new Point[3] { temp[0], arr[1], temp[1] }, stop - 1);
+                    else if(i==2)
+                        draw_wit_tar(g, new Point[3] { temp[2], temp[1], arr[2] }, stop - 1);
+                }
+            }
+                
+        }
+    }
+}
