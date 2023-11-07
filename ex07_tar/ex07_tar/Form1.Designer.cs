43cbff356b90375a483829f1550b03c7c9c0254e
Yang Dunjie <115529614+Fergus4506@users.noreply.github.com>
2023-11-07 17:37:07 +0800

    [upload] ex07 last fun not do

--- ex07_tar/ex07_tar/Form1.Designer.cs
+++ ex07_tar/ex07_tar/Form1.Designer.cs
@@ -31,6 +31,13 @@
             this.start = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.times = new System.Windows.Forms.TextBox();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.check3D = new System.Windows.Forms.CheckBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
             this.SuspendLayout();
             // 
             // start
@@ -46,9 +53,10 @@
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 11F);
             this.label1.Location = new System.Drawing.Point(12, 17);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.Size = new System.Drawing.Size(37, 15);
             this.label1.TabIndex = 1;
             this.label1.Text = "層數";
             // 
@@ -59,17 +67,60 @@
             this.times.Size = new System.Drawing.Size(39, 22);
             this.times.TabIndex = 2;
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Black;
+            this.pictureBox1.Location = new System.Drawing.Point(97, 52);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(27, 25);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("新細明體", 11F);
+            this.label2.Location = new System.Drawing.Point(9, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "三角形顏色";
+            // 
+            // check3D
+            // 
+            this.check3D.AutoSize = true;
+            this.check3D.Location = new System.Drawing.Point(12, 94);
+            this.check3D.Name = "check3D";
+            this.check3D.Size = new System.Drawing.Size(86, 16);
+            this.check3D.TabIndex = 5;
+            this.check3D.Text = "是否畫成3D";
+            this.check3D.UseVisualStyleBackColor = true;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(12, 116);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(104, 45);
+            this.trackBar1.TabIndex = 6;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.check3D);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.times);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.start);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -80,6 +131,11 @@
         private System.Windows.Forms.Button start;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox times;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox check3D;
+        private System.Windows.Forms.TrackBar trackBar1;
     }
 }
 
--- ex07_tar/ex07_tar/Form1.cs
+++ ex07_tar/ex07_tar/Form1.cs
@@ -13,6 +13,7 @@ namespace ex07_tar
     public partial class Form1 : Form
     {
         Graphics g;
+        Color setColor = Color.Black;
         Pen pen = new Pen(Color.Black);
         SolidBrush brush = new SolidBrush(Color.Black);
         public Form1()
@@ -39,6 +40,12 @@ namespace ex07_tar
             //g.FillPolygon(brush,arr);
             if (cnt > 0)
                 draw_black_tar(g,arr,cnt);
+            if (check3D.Checked) {
+                PointF mid = new PointF(400,600);
+                draw_3d_left_part(g, new PointF[3] { new PointF(400, 100), new PointF(0, 900), mid }, cnt, Color.Green);
+                draw_3d_left_part(g, new PointF[3] { mid, new PointF(0, 900), new PointF(800,900) }, cnt, Color.Blue);
+                draw_3d_left_part(g, new PointF[3] { new PointF(400,100),mid, new PointF(800, 900) }, cnt, Color.Red);
+            }
         }
         /*void draw_wit_tar(Graphics g,Point[] arr,int stop) {
             Point[] temp = new Point[3] { new Point((arr[0].X + arr[1].X) / 2, (arr[0].Y + arr[1].Y) / 2), new Point((arr[1].X + arr[2].X) / 2, (arr[1].Y + arr[2].Y) / 2), new Point((arr[2].X + arr[0].X) / 2, (arr[2].Y + arr[0].Y) / 2) };
@@ -72,8 +79,40 @@ namespace ex07_tar
                 }
             }
             else {
-                g.DrawPolygon(new Pen(Color.Black), arr);
-                g.FillPolygon(new SolidBrush(Color.Black), arr);
+                g.DrawPolygon(new Pen(setColor), arr);
+                g.FillPolygon(new SolidBrush(setColor), arr);
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            colorDialog1.ShowDialog();
+            setColor = colorDialog1.Color;
+            pictureBox1.BackColor = setColor;
+            pen.Color = setColor;
+            brush.Color = setColor;
+            start_Click(sender,e);
+        }
+        void draw_3d_left_part(Graphics g,PointF[] arr,int stop,Color c) {
+            PointF[] temp = new PointF[3] { new PointF((arr[0].X + arr[1].X) / 2, (arr[0].Y + arr[1].Y) / 2), new PointF((arr[1].X + arr[2].X) / 2, (arr[1].Y + arr[2].Y) / 2), new PointF((arr[2].X + arr[0].X) / 2, (arr[2].Y + arr[0].Y) / 2) };
+            //g.DrawPolygon(new Pen(Color.White), temp);
+            //g.FillPolygon(new SolidBrush(Color.White), temp);
+            if (stop - 1 > 0)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    if (i == 0)
+                        draw_3d_left_part(g, new PointF[3] { arr[0], temp[0], temp[2] }, stop - 1,c);
+                    else if (i == 1)
+                        draw_3d_left_part(g, new PointF[3] { temp[0], arr[1], temp[1] }, stop - 1,c);
+                    else if (i == 2)
+                        draw_3d_left_part(g, new PointF[3] { temp[2], temp[1], arr[2] }, stop - 1,c);
+                }
+            }
+            else
+            {
+                g.DrawPolygon(new Pen(c), arr);
+                g.FillPolygon(new SolidBrush(c), arr);
             }
         }
     }
