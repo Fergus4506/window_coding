49508422cd6b20924e6ca69663012b363893a755
Yang Dunjie <115529614+Fergus4506@users.noreply.github.com>
2023-11-21 17:04:30 +0800

    [upload] ex08 all fun ac

---
+++ ex08_ran/ex08_ran/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace ex08_ran
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.draw = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.trange_from = new System.Windows.Forms.CheckBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.ranColor = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // draw
+            // 
+            this.draw.Location = new System.Drawing.Point(13, 13);
+            this.draw.Name = "draw";
+            this.draw.Size = new System.Drawing.Size(75, 23);
+            this.draw.TabIndex = 0;
+            this.draw.Text = "畫出來";
+            this.draw.UseVisualStyleBackColor = true;
+            this.draw.Click += new System.EventHandler(this.draw_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(116, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // trange_from
+            // 
+            this.trange_from.AutoSize = true;
+            this.trange_from.Location = new System.Drawing.Point(256, 19);
+            this.trange_from.Name = "trange_from";
+            this.trange_from.Size = new System.Drawing.Size(48, 16);
+            this.trange_from.TabIndex = 2;
+            this.trange_from.Text = "菱形";
+            this.trange_from.UseVisualStyleBackColor = true;
+            this.trange_from.CheckedChanged += new System.EventHandler(this.trange_from_CheckedChanged);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Enabled = false;
+            this.trackBar1.Location = new System.Drawing.Point(13, 42);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(171, 45);
+            this.trackBar1.TabIndex = 3;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // ranColor
+            // 
+            this.ranColor.AutoSize = true;
+            this.ranColor.Checked = true;
+            this.ranColor.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.ranColor.Location = new System.Drawing.Point(311, 19);
+            this.ranColor.Name = "ranColor";
+            this.ranColor.Size = new System.Drawing.Size(72, 16);
+            this.ranColor.TabIndex = 4;
+            this.ranColor.Text = "隨機顏色";
+            this.ranColor.UseVisualStyleBackColor = true;
+            this.ranColor.CheckedChanged += new System.EventHandler(this.ranColor_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 761);
+            this.Controls.Add(this.ranColor);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.trange_from);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.draw);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button draw;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckBox trange_from;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.CheckBox ranColor;
+    }
+}
+
---
+++ ex08_ran/ex08_ran/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ex08_ran
+{
+    public partial class Form1 : Form
+    {
+        Color set_color = Color.Black;
+        Random cc = new Random();
+        int n = -1,checknow=9999;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Text = "2";
+            trackBar1.Maximum = (int)(Math.Pow(9, 2));
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Point start = new Point(100, 100);
+            Pen dfpen = new Pen(set_color);
+            Brush dfbrush = new SolidBrush(set_color);
+            if (trange_from.Checked) {
+                g.TranslateTransform(400.0F, -100.0F);
+                g.RotateTransform(45.0f);
+            }
+            
+            if (n == 0)
+                g.FillRectangle(dfbrush, start.X, start.Y, 540, 540);
+            else if(n>0)
+                draw_ran(g,start,dfpen,dfbrush,180,n,0);
+            
+
+        }
+        int draw_ran(Graphics g,Point start,Pen dfp,Brush dfb,int ransize,int count,int now) {
+            if (count == 0 || now==checknow)
+                return now;
+            set_color = randonColor();
+            dfp.Color = set_color;
+            dfb = new SolidBrush(set_color);
+            Point[] allranStart = new Point[9];
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    allranStart[i * 3 + j] = new Point(start.X+ransize*j,start.Y+ransize*i);
+                }
+            }
+            for (int i = 0; i < 9; i++) {
+                if(i!=4){
+                    g.DrawRectangle(dfp, allranStart[i].X, allranStart[i].Y, ransize, ransize);
+                    now=draw_ran(g, allranStart[i], dfp, dfb, ransize / 3, count - 1,now+1);
+                }
+                else{
+                    g.FillRectangle(dfb,allranStart[i].X,allranStart[i].Y,ransize,ransize);
+                    now += 1;
+                }
+                if (now == checknow)
+                    return now;
+            }
+            return now;
+        }
+        private void draw_Click(object sender, EventArgs e)
+        {
+            n = Int32.Parse(textBox1.Text);
+            trackBar1.Maximum = (int)(Math.Pow(9, n));
+            Invalidate();
+        }
+        Color randonColor() {
+            if (ranColor.Checked == true)
+                return Color.FromArgb(cc.Next(0, 255), cc.Next(0, 255), 50);
+            else
+                return Color.Black;
+        }
+
+        private void trange_from_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            n = Int32.Parse(textBox1.Text);
+            trackBar1.Maximum = (int)(Math.Pow(9,n));
+            checknow = trackBar1.Value;
+            Invalidate();
+        }
+
+        private void ranColor_CheckedChanged(object sender, EventArgs e)
+        {
+            if (ranColor.Checked){
+                checknow = 9999;
+                trackBar1.Enabled = false;
+            }
+            else {
+                trackBar1.Enabled = true;
+            }
+                
+        }
+
+    }
+}
