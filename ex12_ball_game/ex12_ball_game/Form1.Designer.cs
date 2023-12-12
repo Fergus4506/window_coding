b9ee1260c785e9282cbc74d3f3c8396efcd4cc87
楊敦傑 <115529614+Fergus4506@users.noreply.github.com>
2023-12-12 17:59:46 +0800

    hw12/12

---
+++ ex12_ball_game/ex12_ball_game/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace ex12_ball_game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lbl_point = new System.Windows.Forms.Label();
+            this.racket = new System.Windows.Forms.PictureBox();
+            this.lbl_gameover = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.playground = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.racket)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbl_point
+            // 
+            this.lbl_point.AutoSize = true;
+            this.lbl_point.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lbl_point.Location = new System.Drawing.Point(13, 13);
+            this.lbl_point.Name = "lbl_point";
+            this.lbl_point.Size = new System.Drawing.Size(52, 16);
+            this.lbl_point.TabIndex = 0;
+            this.lbl_point.Text = "分數:0";
+            // 
+            // racket
+            // 
+            this.racket.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.racket.Location = new System.Drawing.Point(196, 308);
+            this.racket.Name = "racket";
+            this.racket.Size = new System.Drawing.Size(100, 50);
+            this.racket.TabIndex = 2;
+            this.racket.TabStop = false;
+            // 
+            // lbl_gameover
+            // 
+            this.lbl_gameover.AutoSize = true;
+            this.lbl_gameover.Location = new System.Drawing.Point(334, 172);
+            this.lbl_gameover.Name = "lbl_gameover";
+            this.lbl_gameover.Size = new System.Drawing.Size(33, 12);
+            this.lbl_gameover.TabIndex = 3;
+            this.lbl_gameover.Text = "label1";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // playground
+            // 
+            this.playground.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.playground.Location = new System.Drawing.Point(0, 0);
+            this.playground.Name = "playground";
+            this.playground.Size = new System.Drawing.Size(784, 761);
+            this.playground.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 761);
+            this.Controls.Add(this.lbl_gameover);
+            this.Controls.Add(this.racket);
+            this.Controls.Add(this.lbl_point);
+            this.Controls.Add(this.playground);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.racket)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_point;
+        private System.Windows.Forms.PictureBox ball;
+        private System.Windows.Forms.PictureBox racket;
+        private System.Windows.Forms.Label lbl_gameover;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel playground;
+    }
+}
+
